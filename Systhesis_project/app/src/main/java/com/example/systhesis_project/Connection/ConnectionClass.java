cfebd4618b5277e97a527da1999e50f974a1c8d2
letannam1999 <59557844+letannam1999@users.noreply.github.com>
2021-05-24 20:25:26 +0700

    Login Function

---
+++ Systhesis_project/app/src/main/java/com/example/systhesis_project/Connection/ConnectionClass.java
@@ -0,0 +1,8 @@
+package com.example.systhesis_project.Connection;
+
+public class ConnectionClass {
+    public static String ip="192.168.100.4";
+    public static String un = "NamCce";
+    public static String pass = "123456";
+    public static String db = "DACN";
+}
---
+++ Systhesis_project/app/src/main/java/com/example/systhesis_project/LoginActivity.java
@@ -0,0 +1,118 @@
+package com.example.systhesis_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.systhesis_project.Connection.ConnectionClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText id,pass;
+    Button btnlogin;
+    Connection con;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        id=(EditText)findViewById(R.id.userid);
+        pass=(EditText)findViewById(R.id.userpass);
+        btnlogin=(Button)findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new LoginActivity.checkLogin().execute("");
+            }
+        });
+    }
+    public class checkLogin extends AsyncTask<String, String, String>{
+
+        String z = null;
+        Boolean isSuccess = false;
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onPostExecute(String s) {}
+
+        @Override
+        protected String doInBackground(String... strings) {
+            con = connectionClass(ConnectionClass.un.toString(),ConnectionClass.pass.toString(),ConnectionClass.db.toString(),ConnectionClass.ip.toString());
+            if(con==null){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this,"Check Internet Connection",Toast.LENGTH_LONG).show();
+                    }
+                });
+                z = "On Internet Connection";
+            }else {
+                try {
+                    String sql = "SELECT * FROM Student WHERE StudentId = '" + id.getText() + "' AND Password ='" + pass.getText() + "'";
+                    Statement statement = con.createStatement();
+                    ResultSet rs = statement.executeQuery(sql);
+
+                    if (rs.next()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        z= "Success";
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Check UserId or Password", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        id.setText("");
+                        pass.setText("");
+                    }
+                    con.close();
+                } catch (Exception e) {
+                    isSuccess = false;
+                    Log.e("SQL Error: ", e.getMessage());
+                }
+            }
+            return z;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public Connection connectionClass(String user,String password, String database, String server){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://"+server+"/"+database+";user="+user+";password="+password+";";
+            connection = DriverManager.getConnection(connectionURL);
+        }catch (Exception e){
+            Log.e("SQL Connection Error: ",e.getMessage());
+        }
+        return connection;
+    }
+}
